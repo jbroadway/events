f4e3625d3d04224b334c2c7644bcbd157ab962a3
jbroadway <johnny@johnnybroadway.com>
2011-08-06 14:26:57 -0500

    Sorting admin list by newest first

--- handlers/admin.php
+++ handlers/admin.php
@@ -14,7 +14,7 @@ $_GET['offset'] = (isset ($_GET['offset'])) ? $_GET['offset'] : 0;
 $lock = new Lock ();
 
 $events = Event::query ('id, title, start_date, end_date, starts, ends')
-	->order ('id asc')
+	->order ('start_date desc')
 	->fetch_orig ($limit, $_GET['offset']);
 $count = Event::query ()->count ();
 
