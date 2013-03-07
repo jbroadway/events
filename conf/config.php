bc08befd37229e4650bd74ec2a8684585ad04750
jbroadway <johnny@johnnybroadway.com>
2013-03-06 20:03:09 -0600

    Added sitemap url generation

--- conf/config.php
+++ conf/config.php
@@ -7,5 +7,6 @@ name = Events
 install = events/install
 upgrade = events/upgrade
 version = 0.9-beta
+sitemap = "Event::sitemap"
 
 ; */ ?>
--- models/Event.php
+++ models/Event.php
@@ -16,6 +16,22 @@
  * email
  * phone
  */
-class Event extends Model {}
+class Event extends Model {
+	/**
+	 * Generate a list of pages for the sitemaps app.
+	 */
+	public static function sitemap () {
+		$events = Event::query ()
+			->where ('start_date >= "' . gmdate ('Y-m-01 00:00:00') . '"')
+			->where ('end_date <= "' . gmdate ('Y-m-t 23:59:59') . '"')
+			->fetch_orig ();
+
+		$urls = array ();
+		foreach ($events as $event) {
+			$urls[] = sprintf ('/events/%d/%s', $event->id, URLify::filter ($event->title));
+		}
+		return $urls;
+	}
+}
 
 ?>
