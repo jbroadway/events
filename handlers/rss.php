09c4998da3c0acae06ee030f47e43f9d07293f5f
jbroadway <johnny@johnnybroadway.com>
2013-03-31 19:23:39 -0500

    Added rss output at /events/rss

---
+++ handlers/rss.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Renders the RSS feed for the blog.
+ */
+
+$res = $cache->get ('events_rss');
+if (! $res) {
+	$feed = new StdClass;
+	$feed->title = $appconf['Events']['title'];
+	$feed->date = gmdate ('Y-m-d\TH:i:s');
+	$feed->events = Event::query ()
+		->where ('start_date >= "' . gmdate ('Y-m-d') . '"')
+		->order ('start_date', 'asc')
+		->fetch_orig (10);
+
+	foreach ($feed->events as $k => $event) {
+		$feed->events[$k]->url = '/events/' . $event->id . '/' . URLify::filter ($event->title);
+		$feed->events[$k]->body = $tpl->run_includes ($feed->events[$k]->details);
+	}
+
+	$res = $tpl->render ('events/rss', $feed);
+	$cache->set ('events_rss', $res, 1800); // half an hour
+}
+$page->layout = false;
+header ('Content-Type: text/xml');
+echo $res;
+
+?>
