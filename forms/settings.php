a5b7e96b5001f3a58cf2acb72f54e0fe6c10f1a1
jbroadway <johnny@johnnybroadway.com>
2013-05-26 23:03:18 -0500

    Added settings form

---
+++ forms/settings.php
@@ -0,0 +1,17 @@
+; <?php /*
+
+[title]
+
+not empty = 1
+
+[layout]
+
+skip_if_empty = 1
+callback = admin_layout_exists
+
+[event_layout]
+
+skip_if_empty = 1
+callback = admin_layout_exists
+
+; */ ?>
---
+++ handlers/settings.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This is the settings form for the blog app.
+ */
+
+$this->require_admin ();
+
+require_once ('apps/admin/lib/Functions.php');
+
+$page->layout = 'admin';
+$page->title = __ ('Events Settings');
+
+$form = new Form ('post', $this);
+
+$form->data = array (
+	'title' => $appconf['Events']['title'],
+	'layouts' => admin_get_layouts (),
+	'layout' => $appconf['Events']['layout'],
+	'event_layout' => $appconf['Events']['event_layout'],
+	'gcal_link' => $appconf['Events']['gcal_link']
+);
+
+echo $form->handle (function ($form) {
+	if (! Ini::write (
+		array (
+			'Events' => array (
+				'title' => $_POST['title'],
+				'layout' => $_POST['layout'],
+				'event_layout' => $_POST['event_layout'],
+				'gcal_link' => $_POST['gcal_link']
+			)
+		),
+		'conf/app.events.' . ELEFANT_ENV . '.php'
+	)) {
+		printf ('<p>%s</p>', __ ('Unable to save changes. Check your folder permissions and try again.'));
+		return;
+	}
+
+	$form->controller->add_notification (__ ('Settings saved.'));
+	$form->controller->redirect ('/events/admin');
+});
+
+?>
