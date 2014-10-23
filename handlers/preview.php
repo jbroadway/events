465a9984fc763ee5ba80927cfcd9948c244c8197
jbroadway <johnny@johnnybroadway.com>
2014-10-23 11:54:55 -0500

    Added event previews

---
+++ handlers/preview.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Create a preview of an event based on POST data sent to it.
+ * POST data must match values available to the Event object.
+ */
+
+$this->require_admin ();
+
+require_once ('apps/events/lib/Filters.php');
+
+$e = new Event ($_POST);
+
+$page->id = 'events';
+$page->title = $e->title;
+$page->layout = Appconf::events ('Events', 'event_layout');
+$e->details = $tpl->run_includes ($e->details);
+$e->remaining = $e->available;
+$e->has_passed = ($e->start_date . ' ' . $e->starts) < gmdate ('Y-m-d H:i:s');
+echo $tpl->render ('events/event', $e->orig ());
+
+?>
