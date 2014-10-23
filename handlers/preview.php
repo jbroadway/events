64aa3c62d2bb7bfd685fdd2046fb85a687d4357f
jbroadway <johnny@johnnybroadway.com>
2014-10-23 12:41:01 -0500

    End-date fix on preview

--- handlers/preview.php
+++ handlers/preview.php
@@ -16,6 +16,9 @@ $page->title = $e->title;
 $page->layout = Appconf::events ('Events', 'event_layout');
 $e->details = $tpl->run_includes ($e->details);
 $e->remaining = $e->available;
+if ($e->end_date === '' || $e->end_date === '0000-00-00' || $e->end_date === $e->start_date) {
+	$e->end_date = false;
+}
 $e->has_passed = ($e->start_date . ' ' . $e->starts) < gmdate ('Y-m-d H:i:s');
 echo $tpl->render ('events/event', $e->orig ());
 
